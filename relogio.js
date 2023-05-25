79df64e0857ac0d1e00cc14111e39ee161614055
ronaldokiiller <134619544+ronaldokiiller@users.noreply.github.com>
2023-05-25 16:01:10 -0300

    adicionado o arquivo de script relogio.js

--- relogio.js
+++ relogio.js
@@ -1,1 +1,18 @@
+let d = new Date();
 
+		 const weekday = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"];
+
+		 const month = ["janeiro","fevereiro","março","abril","maio","junho","agosto","julho","setembro","outubro","novembro","dezembro"];
+
+		 let day = weekday[d.getDay()]+", "+ d.getDate() + " de " + month[d.getMonth()]  + " de " + d.getFullYear();
+
+		 let hour = ("0"+d.getHours()).slice(-2) + " : " + ("0"+ d.getMinutes()).slice(-2) + " : " + ("0"+ d.getSeconds()).slice(-2);
+
+		 document.getElementById("day").innerHTML = day;
+
+		 document.getElementById("clock").innerHTML = hour;
+
+		 setTimeout(function(){
+			window.location.reload(1);
+			
+			}, 1000);
